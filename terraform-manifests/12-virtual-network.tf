2b189164c65aa4d570687070567aedf8ae3e4b00
Anhgrew <socksockmoizui@gmail.com>
2023-03-12 15:01:17 +0700

    feat: Upload git ignore terraform init

--- terraform-manifests/12-virtual-network.tf
+++ terraform-manifests/12-virtual-network.tf
@@ -1,6 +1,6 @@
 # Create Virtual Network
 resource "azurerm_virtual_network" "anhgrew-devops" {
-  name                = "aks-network"
+  name                = "anhgrew-devops-network"
   location            = azurerm_resource_group.aks_rg.location
   resource_group_name = azurerm_resource_group.aks_rg.name
   address_space       = ["172.16.0.0/16"]
@@ -8,8 +8,8 @@ resource "azurerm_virtual_network" "anhgrew-devops" {
 
 # Create a Subnet for AKS
 resource "azurerm_subnet" "anhgrew-devops-default" {
-  name                 = "aks-default-subnet"
-  virtual_network_name = azurerm_virtual_network.aksvnet.name
+  name                 = "anhgrew-devops-default-subnet"
+  virtual_network_name = azurerm_virtual_network.anhgrew-devops.name
   resource_group_name  = azurerm_resource_group.aks_rg.name
   address_prefixes     = ["172.16.0.0/17"]
 }
